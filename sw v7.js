3691778793bd238440003039d1916aa40131491a
Drwilo <williamsokolotu@gmail.com>
2026-06-14 12:13:17 -0500

    Add files via upload

---
+++ sw v7.js
@@ -0,0 +1,4 @@
+const V='audhd-v7',F=['./','./sw.js'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(V).then(c=>c.addAll(F)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(n=>n!==V).map(n=>caches.delete(n)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const c=res.clone();caches.open(V).then(ca=>ca.put(e.request,c));return res;})));});
